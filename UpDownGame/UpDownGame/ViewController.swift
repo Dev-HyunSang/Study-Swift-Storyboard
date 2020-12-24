9cd126160fea3a70e2588e200a470736f9a786bf
HyunSang Park <hyun.sang@parkhyunsang.com>
2020-12-24 21:28:05 +0900

    [UPDATE] Challenge - Adding RESET Button
    2020.12.24 완료

--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -8,15 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 요소와 연결
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // 이벤트에 반응
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         print(sender.value)
     
+    }
+    
+    //
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton(_sender: UIButton) {
+        print("touch up reset button")
     }
 }
 
