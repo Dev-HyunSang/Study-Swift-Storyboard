9953c9f5e043948a7e3ab126101e1c9c868f50a0
HyunSang Park <hyun.sang@parkhyunsang.com>
2020-12-25 01:18:10 +0900

    [UPDATE🛠] Slider UPDATE

--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"), for: .normal)
     }
 
     // 이벤트에 반응
