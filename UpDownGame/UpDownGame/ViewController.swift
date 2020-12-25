0842c436476c018f8c91056a37bb3aa681e141cd
HyunSang Park <hyun.sang@parkhyunsang.com>
2020-12-25 15:38:09 +0900

    [UPDATE🛠] Slider Value 오류 해결 및 업데이트 🎄

--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -9,21 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomValue: Int = 0
+    var tryCount: Int = 0
+    
     // 요소와 연결
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var SliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLable: UILabel!
+    @IBOutlet weak var maximumValueLable: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        slider.value = 15
         slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"), for: .normal)
-        reset()
     }
 
     // 이벤트에 반응
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         print(sender.value)
-    
     }
     
     //
@@ -38,6 +44,18 @@ class ViewController: UIViewController {
     
     func reset(){
         print("reset!")
+        // ... => 범위 연산자
+        randomValue = Int.random(in: 0...30)
+        print(randomValue)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLable.text = "0"
+        maximumValueLable.text = "30"
+        SliderValueLabel.text = "15"
     }
+    
 }
 
