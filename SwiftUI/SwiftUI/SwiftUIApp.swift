967e4b0b42d95263211763f8380fadb89ba8f38b
HyunSang Park <hyun.sang@parkhyunsang.com>
2020-12-30 09:24:28 +0900

    [UPDATE🛠] Swift stack pratice tutorial

---
+++ SwiftUI/SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by Jasper Park on 2020/12/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI_stack_pratice_tutorial/SwiftUI_stack_pratice_tutorial/SwiftUI_stack_pratice_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_stack_pratice_tutorialApp.swift
+//  SwiftUI_stack_pratice_tutorial
+//
+//  Created by Jasper Park on 2020/12/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_stack_pratice_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
