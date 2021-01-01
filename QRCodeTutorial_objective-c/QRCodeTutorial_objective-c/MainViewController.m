d8c37328f4e395447c9e60c21a4064aac75d63eb
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-01-01 17:33:55 +0900

    [UPDATE🛠] QRCodeReader Install & QRCode QRCodeReaderDelegate Set UP

--- QRCodeTutorial_objective-c/QRCodeTutorial_objective-c/MainViewController.m
+++ QRCodeTutorial_objective-c/QRCodeTutorial_objective-c/MainViewController.m
@@ -6,9 +6,12 @@
 //
 
 #import "MainViewController.h"
+#import <QRCodeReaderViewController/QRCodeReaderViewController.h>
+#import <QRCodeReader.h>
 
-@interface MainViewController ()
-
+@interface MainViewController () <QRCodeReaderDelegate> {
+    QRCodeReaderViewController *QRCodeVC;
+}
 @end
 
 @implementation MainViewController
@@ -16,18 +19,58 @@
 // View Load / First Loading
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // Create the reader object
+    QRCodeReader *reader = [QRCodeReader readerWithMetadataObjectTypes:@[AVMetadataObjectTypeQRCode]];
+
+    // Instantiate the view controller
+    QRCodeVC = [QRCodeReaderViewController readerWithCancelButtonTitle:@"Cancel" codeReader:_reader startScanningAtLoad:YES showSwitchCameraButton:YES showTorchButton:YES];
+
+    // Set the presentation style
+    QRCodeVC.modalPresentationStyle = UIModalPresentationFormSheet;
+
+    // Define the delegate receiver
+    QRCodeVC.delegate = self;
+
+    // Or use blocks
+    [_reader setCompletionWithBlock:^(NSString *resultAsString) {
+      NSLog(@"%@", resultAsString);
+    }];
+    
     NSURL *websiteUrl = [NSURL URLWithString:@"http://www.naver.com"];
     
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL: websiteUrl];
     
     [_webView loadRequest: urlRequest];
     
+    [_QRCodeBtn layer].borderColor = UIColor.blueColor.CGColor;
+    [_QRCodeBtn layer].borderWidth = 3;
+    [_QRCodeBtn layer].cornerRadius = 10;
+    [_QRCodeBtn layer].backgroundColor = [UIColor yellowColor].CGColor;
+    
+    // QRCodeBtn Click After QRCodeReaderLauch Call
     [_QRCodeBtn addTarget:self action:@selector(QRCodeReaderLaunch) forControlEvents:UIControlEventTouchUpInside];
+        
     
 }
 
 - (void)QRCodeReaderLaunch {
     NSLog(@"MainViewController - QRCodeReaderLaunch() called");
+}
+
+
+#pragma mark - QRCodeReader Delegate Methods
+
+- (void)reader:(QRCodeReaderViewController *)reader didScanResult:(NSString *)result
+{
+  [self dismissViewControllerAnimated:YES completion:^{
+    NSLog(@"%@", result);
+  }];
+}
+
+- (void)readerDidCancel:(QRCodeReaderViewController *)reader
+{
+  [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
 @end
