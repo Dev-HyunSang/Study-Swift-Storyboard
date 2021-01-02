ae08ee4cc8110e9e93c6069e1a927c1310c4d1b4
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-01-02 11:11:10 +0900

    [UPDATE🛠] 제작 완료 !

--- NavigationViewController_tutorial/NavigationViewController_tutorial/Controllers/MainViewController.swift
+++ NavigationViewController_tutorial/NavigationViewController_tutorial/Controllers/MainViewController.swift
@@ -20,14 +20,53 @@ class MainViewController: UIViewController {
         return label
     }()
     
+    let changeBtn : UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("배경색 바꾸기", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        btn.backgroundColor = UIColor.white
+        btn.layer.cornerRadius = 5
+        btn.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        btn.addTarget(self, action: #selector(changeBg), for: .touchUpInside)
+
+        return btn
+    }()
+    
+    var isBgOrange: Bool?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            
+                
+        isBgOrange = true
+        
+        
         view.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
         view.addSubview(titleLabel)
         
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        view.addSubview(changeBtn)
+        
+        changeBtn.translatesAutoresizingMaskIntoConstraints = false
+        changeBtn.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+        changeBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+    }
+    
+        
+    @objc fileprivate func changeBg() {
+        print("MainViewController - CangeBg()")
+        
+        if isBgOrange == true {
+            view.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+            isBgOrange = false
+        }else {
+            view.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+            isBgOrange = true
+        }
+        print("MainViewController - ChangeBg() called / isBgOrange: \(isBgOrange)")
     }
 }
