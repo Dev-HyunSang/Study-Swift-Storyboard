c38ca05dd34a024ffe6e4e45a6a7aa52ecb5655b
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-01-02 09:45:28 +0900

    [UPDATE🛠] if문 추가 & Style Update!

---
+++ NavigationViewController_tutorial/NavigationViewController_tutorial/Controllers/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  NavigationViewController_tutorial
+//
+//  Created by Jasper Park on 2021/01/02.
+//  Copyright © 2021 Tuentuenna. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    //Title
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "메인화면"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 50)
+        label.textColor = UIColor.white
+        return label
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        view.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+          
+    }
+}
